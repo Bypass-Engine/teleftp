cbf3327dcbc369791922f6d75bd254d4ded64036
Vitaly Kryukov <vit110188@gmail.com>
2021-06-13 02:58:45 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,13 @@
+module teleftp
+
+go 1.16
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/goftp/file-driver v0.0.0-20180502053751-5d604a0fc0c9
+	github.com/goftp/server v0.0.0-20200708154336-f64f7c2d8a42
+	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ teleftp.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	bot "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/goftp/file-driver"
+	"github.com/goftp/server"
+	"github.com/joho/godotenv"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	cache []string
+	agent *bot.BotAPI
+)
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Delete(a []string, x string) {
+	index := 0
+	for _, n := range a {
+		if x != n {
+			a[index] = n
+			index++
+		}
+	}
+
+	cache = a[:index]
+}
+
+func Sum(path string) (string, error) {
+	var res string
+	file, err := os.Open(path)
+	if err != nil {
+		return res, err
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return res, err
+	}
+
+	res = hex.EncodeToString(hash.Sum(nil)[:16])
+
+	return res, nil
+}
+
+func ftpHandler() {
+	factory := &filedriver.FileDriverFactory{
+		RootPath: os.Getenv("PATH_FILES"),
+		Perm:     server.NewSimplePerm("root", "root"),
+	}
+
+	port, _ := strconv.Atoi(os.Getenv("FTP_PORT"))
+	opts := &server.ServerOpts{
+		Factory: factory,
+		Auth: &server.SimpleAuth{
+			Name:     os.Getenv("FTP_USER"),
+			Password: os.Getenv("FTP_PASS"),
+		},
+		Hostname: os.Getenv("FTP_HOST"),
+		Port:     port,
+		Logger:   new(server.DiscardLogger),
+	}
+
+	log.Println("FTP is running and listening...")
+
+	if err := server.NewServer(opts).ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func filesHandler() {
+	if _, err := os.Stat(os.Getenv("PATH_FILES")); os.IsNotExist(err) {
+		_ = os.Mkdir(os.Getenv("PATH_FILES"), os.ModeDir)
+	}
+
+	files, err := ioutil.ReadDir(os.Getenv("PATH_FILES"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		full := os.Getenv("PATH_FILES") + f.Name()
+		hash, _ := Sum(full)
+
+		if !Contains(cache, hash) {
+			newHash, _ := Sum(full)
+			if hash != newHash {
+				return
+			}
+
+			cache = append(cache, hash)
+
+			log.Println("↓ |", f.Name(), f.Size(), "bytes")
+
+			chat, _ := strconv.Atoi(os.Getenv("TELEGRAM_CHAT"))
+			c := bot.NewDocumentUpload(int64(chat), full)
+			c.Caption = f.Name()
+			if _, err := agent.Send(c); err != nil {
+				log.Println(err)
+			}
+
+			log.Println("↑ |", f.Name(), f.Size(), "bytes")
+
+			if err := os.RemoveAll(full); err != nil {
+				log.Fatal(err)
+			}
+
+			Delete(cache, hash)
+		}
+	}
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No \".env\" file found!")
+	}
+
+	go ftpHandler()
+
+	var err error
+	agent, err = bot.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		filesHandler()
+
+		time.Sleep(1 * time.Second)
+	}
+}
