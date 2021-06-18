797c3d989637f460e8882268fc88e9569030dbf5
Vitaly Kryukov <vit110188@gmail.com>
2021-06-19 00:11:42 +0300

    Other improvements

--- go.mod
+++ go.mod
@@ -3,10 +3,18 @@ module teleftp
 go 1.16
 
 require (
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/frankban/quicktest v1.13.0 // indirect
 	github.com/goftp/file-driver v0.0.0-20180502053751-5d604a0fc0c9
 	github.com/goftp/server v0.0.0-20200708154336-f64f7c2d8a42
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/mholt/archiver v3.1.1+incompatible
+	github.com/nwaples/rardecode v1.1.0 // indirect
+	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	gopkg.in/tucnak/telebot.v2 v2.3.5
 )
--- teleftp.go
+++ teleftp.go
@@ -5,107 +5,20 @@
 package main
 
 import (
-	"archive/zip"
 	"github.com/goftp/file-driver"
 	"github.com/goftp/server"
 	"github.com/joho/godotenv"
+	"github.com/mholt/archiver"
 	tb "gopkg.in/tucnak/telebot.v2"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strconv"
 	"time"
 )
 
 var agent *tb.Bot
-
-func Archive(source, target string) error {
-	file, err := os.Create(target)
-	if err != nil {
-		return err
-	}
-
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(file)
-
-	w := zip.NewWriter(file)
-	defer func(w *zip.Writer) {
-		err := w.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(w)
-
-	walker := func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-
-		defer func(file *os.File) {
-			err := file.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}(file)
-
-		f, err := w.Create(path)
-		if err != nil {
-			return err
-		}
-
-		if _, err = io.Copy(f, file); err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	if err = filepath.Walk(source, walker); err != nil {
-		return err
-	}
-
-	if err = os.RemoveAll(source); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func IsEmpty(name string) (bool, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return false, err
-	}
-
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(f)
-
-	if _, err = f.Readdirnames(1); err == io.EOF {
-		return true, nil
-	}
-
-	return false, err
-}
 
 func checkBackupProc() bool {
 	if out, err := exec.Command("sh", "-c", "pidof fastbackup").Output(); err == nil && out != nil {
@@ -143,26 +56,19 @@ func ftpHandler() {
 func filesHandler() {
 	if files, err := ioutil.ReadDir(os.Getenv("PATH_FILES")); err == nil {
 		for _, f := range files {
-			fullPath := os.Getenv("PATH_FILES") + f.Name()
-
 			if f.IsDir() {
-				if e, err := IsEmpty(fullPath); err == nil {
-					if e {
-						return
-					}
-				} else {
-					log.Fatal(err)
-				}
+				fullPath := os.Getenv("PATH_FILES") + f.Name()
+				fullPathZip := fullPath + ".zip"
 
 				log.Println("↓ |", f.Name())
 
-				if err := Archive(fullPath, fullPath+".zip"); err != nil {
+				if err := archiver.Archive([]string{fullPath}, fullPathZip); err != nil {
 					log.Fatal(err)
 				}
 
 				chat, _ := strconv.Atoi(os.Getenv("TELEGRAM_CHAT"))
 				if _, err := agent.Send(&tb.Chat{ID: int64(chat)}, &tb.Document{
-					File:     tb.FromDisk(fullPath + ".zip"),
+					File:     tb.FromDisk(fullPathZip),
 					FileName: f.Name() + ".zip",
 				}); err != nil {
 					log.Fatal(err)
@@ -170,7 +76,11 @@ func filesHandler() {
 
 				log.Println("↑ |", f.Name())
 
-				if err := os.RemoveAll(fullPath + ".zip"); err != nil {
+				if err := os.RemoveAll(fullPath); err != nil {
+					log.Fatal(err)
+				}
+
+				if err := os.RemoveAll(fullPathZip); err != nil {
 					log.Fatal(err)
 				}
 			}
@@ -202,7 +112,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	go agent.Start()
+	go func() {
+		agent.Start()
+
+		m, _ := strconv.Atoi(os.Getenv("TELEGRAM_CHAT"))
+		if _, err := agent.Send(&tb.Chat{ID: int64(m)}, "[teleftp] FTP is running and listening..."); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	c := false
 	for {
